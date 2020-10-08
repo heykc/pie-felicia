6a5abcec8e35fe263705b1697554624da9c28213
KevinW90 <kevinjwilliams76@gmail.com>
2020-10-08 16:58:30 -0400

    item info displays for product page

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 
 import TopNav from './TopNav';
+import Rating from './Rating';
 
 const ProductPage = (props) => {
   console.log(props)
   return (
     <div className="product-page">
       <TopNav />
+      {props.data.name}
+      <Rating rating={props.data.rating} />
+      {props.data.description}
+      {props.data.sizes.map((size, i) => (
+        <div key={i}>
+          {size}{props.data.prices[i]}
+        </div>
+      ))}
     </div>
   )
 }
