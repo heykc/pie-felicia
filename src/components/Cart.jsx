b1752f10001e49e2511d59266dde2dec2a962af3
KevinW90 <kevinjwilliams76@gmail.com>
2020-10-09 13:10:31 -0400

    comments for cart component

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,26 +1,39 @@
 import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 
+/**
+ * @summary Prints the order
+ * 
+ * @param {array} cart
+ * @param {object} history
+ * @param {string} category
+ * @param {object} shopData 
+ */
 const Cart = ({cart, history, category, shopData}) => {
-  console.log(cart)
-  console.log(history)
   return (
     <div className="cart">
       <div>CART</div>
       <button onClick={() => history.goBack()}>Back to {category}</button>
       <div>Did you remember your <Link to='/supplies'>Supplies</Link> and <Link to='/extras'>Extras</Link>?</div>
+      {/* look through the cart */}
       {cart.map((item, i) => {
-        console.log(item);
+        // pull cart data
+        const {order, product: productName, type} = item;
         return (
           <Fragment key={i}>
-            <div>{item.product}</div>
-            {Object.keys(item.order).map((size, i) => {
-              let a = shopData[item.type].items.find(x => x.name === item.product)
-              if (item.order[size] > 0)
+            <div>{productName}</div>
+            {/* look through the order */}
+            {Object.keys(order).map((size, i) => {
+              let amount = order[size];
+              // allows access to prices
+              let shopItem = shopData[type].items.find(x => x.name === productName);
+              let total = shopItem.prices[i] * amount;
+              // only display if ordered at least 1 of the sizes
+              if (amount > 0)
                 return (<div key={size}>
                           Size: {size}
-                          x{item.order[size]}
-                          ${a.prices[i]*item.order[size]}
+                          x{amount}
+                          ${total}
                         </div>)
             })}
           </Fragment>
